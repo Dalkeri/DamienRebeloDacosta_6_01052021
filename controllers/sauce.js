18735e0ec4c0d7dc89da18d992cba53f10fe4c2e
Damien Rebelo Da costa <rebelo.dacosta.damien@gmail.com>
2021-05-17 01:19:32 +0200

    ajout like / dislike

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -76,69 +76,48 @@ exports.like = (req, res, next) => {
     _id: req.params.id
   }).then(
     (sauce) => {
-      console.log(sauce);
-      // if(sauce.usersLiked.includes(req.body.userIdFromToken)){
-      //   console.log("1");
-      //   if(!req.body.like){
-      //     console.log("11");
-      //     sauce.usersLiked.splice(req.body.userIdFromToken);
-      //   } else if (req.body.like == -1){
-      //     console.log("12");
-      //     sauce.usersLiked.splice(req.body.userIdFromToken);
-      //     sauce.usersDisliked.push(req.body.userIdFromToken);
-      //   }
-      // } else if(sauce.usersDisliked.includes(req.body.userIdFromToken)){
-      //   console.log("2");
-      //   if(!req.body.like){
-      //     console.log("21");
-      //     sauce.usersDisliked.splice(req.body.userIdFromToken);
-      //   } else if (req.body.like){
-      //     console.log("22");
-      //     sauce.usersDisliked.splice(req.body.userIdFromToken);
-      //     sauce.usersLiked.push(req.body.userIdFromToken);
-      //   }
-      // } else {
-        console.log("3", req.body);
-        if(req.body.like){
-          console.log("31");
-          sauce.usersLiked.push(req.body.userIdFromToken);
-        } else if(req.body.like == -1){
-          console.log("32");
-          sauce.usersDisliked.push(req.body.userIdFromToken);
-        }
-      // }
       console.log(req.body);
-      console.log(sauce);
-    //   Sauce.updateOne(
-    //     { _id: req.params.id },
-    //     { 
-    //       $push: { usersLiked: req.body.userIdFromToken },
-    //       $inc:  { likes: 1} 
-    //     }
-    //  )
-    //  .then(
-    //   () => res.status(200).json({ message: 'like modifié !'})
-    //  )
+      let operations = {};
+      if(sauce.usersLiked.includes(req.body.userIdFromToken)){
+        if(!req.body.like){ 
+          operations["$pull"] = { usersLiked: req.body.userIdFromToken }
+          operations["$inc"] = { likes: -1 }
+        } else if (req.body.like == -1){
+          operations["$pull"] = { usersLiked: req.body.userIdFromToken }
+          operations["$push"] = { usersDisliked: req.body.userIdFromToken }
+          operations["$inc"] = { likes: -1, dislikes: 1 }
+          
+        }
+      } else if (sauce.usersDisliked.includes(req.body.userIdFromToken)) {
+        if(!req.body.like){
+          operations["$pull"] = { usersDisliked: req.body.userIdFromToken }
+          operations["$inc"] = { dislikes: -1 }
+        } else if (req.body.like) {
+          operations["$pull"] = { usersDisliked: req.body.userIdFromToken }
+          operations["$push"] = { usersLiked: req.body.userIdFromToken }
+          operations["$inc"] = { dislikes: -1, likes: 1 }
+        }
+      } else {
+        if (req.body.like == -1) {
+          console.log("ICI");
+          operations["$push"] = { usersDisliked: req.body.userIdFromToken }
+          operations["$inc"] = { dislikes: 1}
+        } else if(req.body.like){
+          console.log("LA");
+          operations["$push"] = { usersLiked: req.body.userIdFromToken }
+          operations["$inc"] = { likes: 1 }
+        }
+      }
 
-      // console.log("ici", sauce);
-      Sauce.updateOne({ _id: req.params.id }, { ...sauce })
-      .then(
-        () => res.status(200).json({ message: 'like modifié !'})
-       )
-      // res.status(200).json({ message: 'like modifié !'})
+      console.log(operations);
+
+      Sauce.updateOne(
+        { _id: req.params.id },
+        operations
+     )
+     .then(
+      () => res.status(200).json({ message: 'like modifié !'})
+     )
     }
   )
-  // .then(
-  //   (sauce) => {
-  //     Sauce.updateOne({ _id: req.params.id }, { ...sauce, _id: req.params.id })
-  //   }
-  // )
-  // .then(() => res.status(200).json({ message: 'like modifié !'}))
-  // .catch(
-  //   (error) => {
-  //     res.status(404).json({
-  //       error: error
-  //     });
-  //   }
-  // );
 };
