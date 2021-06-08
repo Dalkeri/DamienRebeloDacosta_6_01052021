a555e1e4fd27334dea52a7429262525d958827d9
Dalkeri <rebelo;dacosta.damien@gmail.com>
2021-06-08 09:46:01 +0200

    change name of method

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -58,7 +58,7 @@ exports.deleteSauce = (req, res, next) => {
 };
 
 
-exports.getAllStuff = (req, res, next) => {
+exports.getAllSauces = (req, res, next) => {
   Sauce.find()
     .then( sauces => res.status(200).json(sauces))
     .catch( error => res.status(400).json({error: error }));
--- routes/sauce.js
+++ routes/sauce.js
@@ -7,7 +7,7 @@ const multer = require('../middleware/multer-config');
 
 const sauceCtrl = require('../controllers/sauce');
 
-router.get('/', auth, sauceCtrl.getAllStuff);
+router.get('/', auth, sauceCtrl.getAllSauces);
 router.post('/', auth, multer, sauceCtrl.createSauce); 
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/:id/like', auth, sauceCtrl.like);
